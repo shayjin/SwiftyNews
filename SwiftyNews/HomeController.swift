28087bf11f23422ffef767e69cc5ec86085f0fda
Jay Shin <shin.810@osu.edu>
2023-02-22 13:59:20 -0500

    내비게이션을 tab bar controller로 규현

--- SwiftyNews/HomeController.swift
+++ SwiftyNews/HomeController.swift
@@ -7,9 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
     @IBOutlet var newsType: UISegmentedControl!
-    @IBOutlet var navBar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +16,6 @@ class ViewController: UIViewController {
         newsType.setTitle("Local", forSegmentAt: 0)
         newsType.setTitle("US", forSegmentAt: 1)
         newsType.setTitle("World", forSegmentAt: 2)
-        
-        navBar.items![0].title = "Home"
-        navBar.items![1].title = "Search"
-        navBar.items![2].title = "Notification"
-        navBar.items![3].title = "Profile"
     }
     
     @IBAction func switchNewsType(_ sender: Any) {
@@ -38,5 +32,7 @@ class ViewController: UIViewController {
             print("Default")
         }
     }
+    
+    
     
 }
