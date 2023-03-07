25aa1f26275a0874f149b857c035b45b639e29de
Jay Shin <shin.810@osu.edu>
2023-03-07 17:37:18 -0500

    사인업이 된 상태면 사인업 페이지 사라짐

--- SwiftyNews/ProfileController.swift
+++ SwiftyNews/ProfileController.swift
@@ -11,19 +11,35 @@ import FirebaseAuth
 class ProfileController: UIViewController {
 
     @IBOutlet var emailTextField: UITextField!
-
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var retypePasswordTextField: UITextField!
+    @IBOutlet var signUpButton: UIButton!
+    @IBOutlet var signUpLogo: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var passwordLabel: UILabel!
+    @IBOutlet var retypePasswordLabel: UILabel!
+    
+    let auth = Auth.auth()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("profile")
+        
+        let signUpElements = [emailTextField, nameTextField, passwordTextField, retypePasswordTextField, signUpButton, signUpLogo, emailLabel, nameLabel, passwordLabel, retypePasswordLabel]
+        
+        if let email = auth.currentUser?.email {
+            print(email)
+            
+            for element in signUpElements {
+                element!.isHidden = true
+            }
+        } else {
+            
+        }
     }
     
     @IBAction func signUp(_ sender: UIButton) {
-
-        
         if (passwordTextField.text != retypePasswordTextField.text) {
             let alertController = UIAlertController(title: "Unmatched Password", message: "Passwords are not matched!", preferredStyle: .alert)
             let action = UIAlertAction(title: "Continue", style: .default, handler: nil)
@@ -39,6 +55,7 @@ class ProfileController: UIViewController {
                 }
                 
                 Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!)
+                print(Auth.auth().currentUser?.email)
                 print("Test user created successfully")
 
             }
