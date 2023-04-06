b0fc79a39adb83af5fea4146129aeceadf71306d
Jay Shin <shin.810@osu.edu>
2023-04-05 22:59:26 -0400

    UI업뎃

---
+++ SwiftyNews/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  SwiftyNews
+//
+//  Created by Shay on 4/5/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet var searchButton: UIButton!
+    @IBOutlet var textField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
